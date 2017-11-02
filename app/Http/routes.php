e18b7f27f92eab1153ed1b90007393c8ecbfe7e7
diegoerleyduran <diegoerleyduran@yahoo.com>
2017-11-02 10:04:26 -0500

    Soft DELETE

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,11 +12,10 @@
 */
 
 Route::get('/', function () {
-   // return AdvancedELOQUENT\Book::all();
-
-return 'HOLA';
+    return AdvancedELOQUENT\Book::all();
 
 
+    
 });
 
 /*
@@ -32,4 +31,29 @@ return 'HOLA';
 
 Route::group(['middleware' => ['web']], function () {
     //
+});
+
+
+// Buscar un registro que está en papelera
+Route::get('registro-en-papelera/{id}', function ($id) {
+    $book = Book::withTrashed()->find($id); 
+    return $book;
+});
+// Enviar un registro a papelera
+Route::get('enviar-a-papelera/{id}', function ($id) {
+	$book = Book::find($id);
+    $book->delete();
+    return 'Enviado a papelera';
+});
+// Restaurar un registro que está en papelera
+Route::get('restaurar-registro/{id}', function ($id) {
+    $book = Book::withTrashed()->find($id);
+    $book->restore();
+    return 'Restaurado';
+});
+// Eliminar un registro de forma permanente
+Route::get('eliminar-registro/{id}', function ($id) {
+    $book = Book::withTrashed()->find($id);
+    $book->forceDelete();
+    return 'Eliminado de forma permanente';
 });
